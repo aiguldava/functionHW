50f3bec0cface51e3757827c47246e7dcce9d133
aiguldava <85512101+aiguldava@users.noreply.github.com>
2021-07-13 15:16:22 +0600

    homework31
    homework31

---
+++ script.js
@@ -0,0 +1,43 @@
+// Домашка 1. CALL мн
+
+// let elem = document.getElementById('elem');
+// function func() {
+//     alert(this.value);
+// }
+// func.call(elem);
+
+// Домашка 2. 
+
+// let elem = document.getElementById('elem');
+
+// function func(surname, name) {
+// alert(this.value + ', ' + surname + ' ' + name);
+// }
+// func.call(elem, 'davlyatova', 'aigul');
+
+//Домашка 3. Apply мн  
+
+//1
+// let elem = document.getElementById('elem');
+// function func() {
+//     alert(this.value);
+// }
+// func.apply(elem, ['value']);
+
+//2
+// let elem = document.getElementById('elem');
+// function func(surname, name) {
+// alert(this.value + ', ' + surname + ' ' + name);
+// }
+// func.apply(elem, ['davlyatova', 'aigul']);
+
+//Домашка 4 bind мн 
+
+// let elem = document.getElementById('elem');
+
+// function func(surname, name) {
+// alert(this.value + ', ' + surname + ' ' + name);
+// }
+// let funcb = func.bind(elem);
+// funcb('davlyatova', 'aigul');
+// funcb('davlyatova' , 'nulifar');
